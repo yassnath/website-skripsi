65d1a1277cc007027d4d38910524bb7af86b0e47
yassnath <andreasnath04@gmail.com>
2025-12-29 19:33:29 +0700

    fix fe + be 4

---
+++ laravel/app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Trust all proxies (Railway / Cloudflare / Vercel)
+     */
+    protected $proxies = '*';
+
+    /**
+     * Use all forwarded headers.
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
+}
