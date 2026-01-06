1ed73f64616872f99779cb0cf294fbe71c0599de
yassnath <andreasnath04@gmail.com>
2026-01-06 14:14:31 +0700

    fixing AI

--- nextJs/src/components/ChatbotWidget.jsx
+++ nextJs/src/components/ChatbotWidget.jsx
@@ -1452,12 +1452,12 @@ const ChatbotWidget = () => {
         }
 
         .cvant-chatbot__input textarea::placeholder {
-          font-size: 12px !important;
+          font-size: 13px !important;
           line-height: 1.2;
         }
 
         .cvant-chatbot__input textarea::-webkit-input-placeholder {
-          font-size: 12px !important;
+          font-size: 13px !important;
           line-height: 1.2;
         }
 
